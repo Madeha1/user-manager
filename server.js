5ff982dbca911dab17c2c551f0979f321efb7488
Madeha Tahboub <madehatahboub@gmail.com>
2021-03-18 21:30:52 +0200

    deploy

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/user-maneger'));
+app.use(express.static(__dirname + '/dist/userManeger'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/user-maneger/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/userManeger/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
